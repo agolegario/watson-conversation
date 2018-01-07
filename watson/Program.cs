8b77306ef0d2eac1ad3038cadb7e2f79504635fd
Adalberto Olegario <agolegario@live.com>
2018-01-07 16:19:38 -0200

    Add project files.

---
+++ watson/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using IBM.WatsonDeveloperCloud.Conversation.v1;
+using IBM.WatsonDeveloperCloud.Conversation.v1.Model;
+using Microsoft.Extensions.Configuration;
+
+namespace watson
+{
+    internal class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        private static void Main(string[] args = null)
+        {
+            Configuration = GetConfiguration();
+
+            var userName = Configuration.GetSection("AppConfiguration")["UserName"];
+            var password = Configuration.GetSection("AppConfiguration")["Password"];
+            var workspaceId = Configuration.GetSection("AppConfiguration")["WorkspaceId"];
+            var dateVersion = Configuration.GetSection("AppConfiguration")["DateVersion"];
+
+            Console.WriteLine("DIGITE exit PARA SAIR");
+
+            var conversation = new ConversationService(userName, password, dateVersion);
+
+            var messageRequest = new MessageRequest {Input = new InputData {Text = ""}};
+            var response = conversation.Message(workspaceId, messageRequest);
+            Console.WriteLine("Watson >> {0}", response.Output.Text.FirstOrDefault());
+
+            while (true)
+            {
+                Console.Write("Usuario >> ");
+                messageRequest.Input = new InputData { Text = Console.ReadLine() };
+                if(messageRequest.Input.Text == "exit") goto Finish;
+                messageRequest.Context = response.Context;
+                response = conversation.Message(workspaceId, messageRequest);
+                Console.WriteLine("Watson >> {0}", response.Output.Text.FirstOrDefault());
+            }
+            Finish: Console.WriteLine("SAINDO DA CONVERSA.........");
+            Console.ReadKey();
+        }
+
+        public static IConfigurationRoot GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            return builder.Build();
+        }
+    }
+}
